08dbb6ba3bfb7e5c9b7beea3e3c887e9c1e3672a
Etienne Rossignon <etienne.rossignon@sterfive.com>
2026-07-07 11:34:12 +0200

    refactor: replace yargs with commander
    Swap yargs for commander to align with project naming and modernize CLI argument parsing. Maintain existing option support and help text.

--- lib/index.ts
+++ lib/index.ts
@@ -42,80 +42,53 @@ async function check_nodejs() {
 // xx const updateNotifier = require("update-notifier");
 const pkg = packageJson;
 
-import yargs from "yargs";
-import { hideBin } from "yargs/helpers";
+import { program } from "commander";
 
-const argv = (yargs(hideBin(process.argv)) as any)
-  .wrap(132)
+program
+  .version(version)
+  .option("-e, --endpoint <url>", "the end point to connect to", "opc.tcp://localhost:26543")
+  .option("-s, --securityMode <mode>", "the security mode (None, Sign, SignAndEncrypt)", "None")
+  .option("-P, --securityPolicy <policy>", "the policy mode (None, Basic128Rsa15, Basic256, etc.)", "None")
+  .option("-u, --userName <name>", "specify the user name of a UserNameIdentityToken")
+  .option("-p, --password <password>", "specify the password of a UserNameIdentityToken")
+  .option("-n, --node <nodeId>", "the nodeId of the value to monitor")
+  .option("-h, --history <nodeId>", "make an historical read")
+  .option("-c, --userCertificate <path>", "X509 user certificate (PEM format)")
+  .option("-x, --userCertificatePrivateKey <path>", "X509 private key associated with the user certificate")
+  .option("-v, --verbose", "display extra information")
+  .addHelpText("after", `
+Examples:
+  opcua-commander --endpoint opc.tcp://localhost:49230 -P=Basic256 -s=Sign
+  opcua-commander -e opc.tcp://localhost:49230 -P=Basic256 -s=Sign -u JoeDoe -p P@338@rd 
+  opcua-commander --endpoint opc.tcp://localhost:49230 -n="ns=0;i=2258"
+`);
 
-  .demand("endpoint")
-  .string("endpoint")
-  .describe("endpoint", "the end point to connect to ")
+program.parse(process.argv);
 
-  .string("securityMode")
-  .describe("securityMode", "the security mode")
+const options = program.opts();
 
-  .string("securityPolicy")
-  .describe("securityPolicy", "the policy mode")
-
-  .string("userName")
-  .describe("userName", "specify the user name of a UserNameIdentityToken ")
-
-  .string("password")
-  .describe("password", "specify the password of a UserNameIdentityToken")
-
-  .string("node")
-  .describe("node", "the nodeId of the value to monitor")
-
-  .string("history")
-  .describe("history", "make an historical read")
-
-  .string("userCertificate")
-  .describe("userCertificate", "X509 user certificate (PEM format)")
-
-  .string("userCertificatePrivateKey")
-  .describe("userCertificatePrivateKey", "X509 private key associated with the user certificate")
-
-  .boolean("verbose")
-  .describe("verbose", "display extra information")
-
-  .alias("e", "endpoint")
-  .alias("s", "securityMode")
-  .alias("P", "securityPolicy")
-  .alias("u", "userName")
-  .alias("p", "password")
-  .alias("n", "node")
-  .alias("t", "timeout")
-  .alias("v", "verbose")
-  .alias("c", "userCertificate")
-  .alias("x", "userCertificatePrivateKey")
-
-  .example("opcua-commander  --endpoint opc.tcp://localhost:49230 -P=Basic256 -s=Sign")
-  .example("opcua-commander  -e opc.tcp://localhost:49230 -P=Basic256 -s=Sign -u JoeDoe -p P@338@rd ")
-  .example('opcua-commander  --endpoint opc.tcp://localhost:49230  -n="ns=0;i=2258"').argv;
-
-const securityMode: MessageSecurityMode = MessageSecurityMode[argv.securityMode || "None"] as any as MessageSecurityMode;
-if (!securityMode) {
+const securityMode: MessageSecurityMode = MessageSecurityMode[options.securityMode || "None"] as any as MessageSecurityMode;
+if (securityMode === undefined) {
   throw new Error(
-    `Invalid Security mode , was  ${chalk.magenta((argv as any).securityMode)}\nshould be  ${chalk.cyan(
+    `Invalid Security mode , was  ${chalk.magenta(options.securityMode)}\nshould be  ${chalk.cyan(
       Object.values(MessageSecurityMode).filter((v) => typeof v === "string").join(",")
     )}`
   );
 }
 
-const securityPolicy = (SecurityPolicy as any)[(argv as any).securityPolicy || "None"];
+const securityPolicy = (SecurityPolicy as any)[options.securityPolicy || "None"];
 if (!securityPolicy) {
   throw new Error(
-    `Invalid securityPolicy\nwas       : ${chalk.magenta((argv as any).securityPolicy)}\nshould be : ${chalk.cyan(
+    `Invalid securityPolicy\nwas       : ${chalk.magenta(options.securityPolicy)}\nshould be : ${chalk.cyan(
       Object.keys(SecurityPolicy).filter((k) => typeof k === "string" && k !== "Invalid" && !k.match(/PubSub/))
     )}`
   );
 }
 
-const endpointUrl = (argv as any).endpoint || "opc.tcp://localhost:26543";
+const endpointUrl = options.endpoint;
+const argv = options;
 if (!endpointUrl) {
-  (yargs as any).showHelp();
-  // xx updateNotifier({ pkg }).notify();
+  program.help();
   process.exit(0);
 }
 
