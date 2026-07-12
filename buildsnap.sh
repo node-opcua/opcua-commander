08b6151f018095d2a50016dcc9b55c1ffdd8ad79
Etienne Rossignon <etienne.rossignon@sterfive.com>
2026-07-12 11:14:52 +0200

    fix(snap): fix quotes in VERSION extraction and correct typo in buildsnap.sh
    - Uses \jq -r\ instead of \jq\ to extract the package version without quotes.
    - This ensures generated snap filenames printed at the end do not contain double quotes.
    - Fixes the 'nstall' typo to 'install'.

--- buildsnap.sh
+++ buildsnap.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 npm run build
 npm run bundle
-VERSION=`jq '.version' package.json`
-echo version  from package.json = $VERSION
+VERSION=`jq -r '.version' package.json`
+echo version from package.json = $VERSION
 
 echo "----------------------------------------------------------------------"
-sed -i "s/^version: .*/version: ${VERSION}/" snap/snapcraft.yaml
+sed -i "s/^version: .*/version: \"${VERSION}\"/" snap/snapcraft.yaml
 cat snap/snapcraft.yaml
 echo "----------------------------------------------------------------------"
 npm install
 snapcraft 
-echo  to nstall locally:  snap install  opcua-commander_${VERSION}_amd64.snap --dangerous
+echo  to install locally:  snap install  opcua-commander_${VERSION}_amd64.snap --dangerous
 echo  to publish:  snapcraft upload --release=stable opcua-commander_${VERSION}_amd64.snap
+
 
